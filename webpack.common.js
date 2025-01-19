5b8e4b7e834259bbc7ad9eb84bedccf3c32173ea
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-01-19 21:25:47 +0200

    Refactored webpack.config.js into dev and prod files.

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,12 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -35,7 +30,7 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
-            },    
+            },
         ],
     },
-}
+};
