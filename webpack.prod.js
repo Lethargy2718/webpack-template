26e79ce947bde982c1af4eef25d072528129fbba
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-03 16:45:22 +0200

    Add Babel

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,17 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
+    rules: [
+        {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    targets: "defaults",
+                    presets: [["@babel/preset-env"]],
+                },
+            },
+        },
+    ],
 });
